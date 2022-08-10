a767a9140b7ca154441a85cb7f949a8ff1f5d343
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-10 22:27:02 +0530

    openzeppelin setup

--- contracts/SVGNFT.sol
+++ contracts/SVGNFT.sol
@@ -1,3 +1,12 @@
 //give the contract some svg code
 //output an NFT uri with this svg code
 //storing all the NFT metadata on-chain
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+contract SVGNFT is ERC721URIStorage{
+    constructor() ERC721("SVG NFT", "svgNFT") {
+        
+    }
+}
