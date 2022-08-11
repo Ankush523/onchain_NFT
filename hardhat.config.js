6c5e848f78feedc3367a8de1be15b0c7b2175c5a
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-11 19:02:03 +0530

    deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("@nomiclabs/hardhat-ethers")
+require("hardhat-deploy")
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks:{
+    hardhat: { },
+    // rinkeby: {
+    //   accounts:
+    // }
+  }, 
   solidity: "0.8.9",
+  namedAccounts: {
+    deployer:{
+      default: 0
+    }
+  }
 };
---
+++ helper-hardhat-config.js
@@ -0,0 +1,9 @@
+const networkConfig = {
+    31337:{
+        name: 'localhost'
+    },
+    4 : {
+         name: 'rinkeby'
+    }
+}
+module.exports = {networkConfig}
